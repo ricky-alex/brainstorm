0ea7e6de212a8c839f73ca041878a8e4a5202328
ricky-alex <rickyalexji@gmail.com>
2026-01-25 17:19:17 +0800

    feat: core module exports

---
+++ brainstorm/core/__init__.py
@@ -0,0 +1,7 @@
+"""Core module -- the foundation of Brainstorm."""
+
+from brainstorm.core.engine import Engine
+from brainstorm.core.pipeline import Pipeline
+from brainstorm.core.module import Module
+
+__all__ = ["Engine", "Pipeline", "Module"]
