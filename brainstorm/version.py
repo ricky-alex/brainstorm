29d24454189e475caaf9267360b4ed021df7f568
ricky-alex <rickyalexji@gmail.com>
2026-04-19 08:11:25 +0800

    feat: add version info and banner

---
+++ brainstorm/version.py
@@ -0,0 +1,12 @@
+"""Version information."""
+
+__version__ = "0.1.0"
+__version_info__ = tuple(int(x) for x in __version__.split("."))
+
+BANNER = """
++---------------------------------------+
+|         B R A I N S T O R M           |
+|     The Core AI Framework             |
+|     Version {v}                        |
++---------------------------------------+
+""".format(v=__version__)
